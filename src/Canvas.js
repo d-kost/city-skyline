bd1a93b2c3e69e4cc5d6e19c926e32a43cc42c1f
Daria Kostuchenko <dariakostuchenko90@gmail.com>
2022-11-13 22:59:26 +0800

    redraw on button click

---
+++ src/Canvas.js
@@ -0,0 +1,17 @@
+export default class Canvas {
+  constructor(id) {
+    this.domCanvas = document.getElementById(id);
+    this.domCanvas.width = window.innerWidth;
+    this.domCanvas.height = window.innerHeight;
+    this.ctx = this.domCanvas.getContext('2d');
+  }
+
+  setCanvasBackground() {
+    const grd = this.ctx.createLinearGradient(0, 0, 0, this.domCanvas.height);
+    grd.addColorStop(0, '#80deea');
+    grd.addColorStop(0.37, '#ffccbc');
+    grd.addColorStop(1, '#ff8a80');
+    this.ctx.fillStyle = grd;
+    this.ctx.fillRect(0, 0, this.domCanvas.width, this.domCanvas.height);
+  }
+}
--- src/script.js
+++ src/script.js
@@ -1,20 +1,27 @@
 import { BuildingDrawer } from './BuildingDrawer';
 import { BuildingGenerator } from './BuildingGenerator';
+import Canvas from './Canvas';
 
-const canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const ctx = canvas.getContext('2d');
+const generate = (ctx) => {
+  const startPos = [10, 100];
+  const buildingGenerator = new BuildingGenerator(...startPos);
+  const buildingDrawer = new BuildingDrawer(ctx, buildingGenerator);
+  buildingDrawer.setInitialColor([231, 44, 74]);
+  buildingDrawer.drawCity();
+};
 
-const grd = ctx.createLinearGradient(0, 0, 0, canvas.height);
-grd.addColorStop(0, '#80deea');
-grd.addColorStop(0.37, '#ffccbc');
-grd.addColorStop(1, '#ff8a80');
-ctx.fillStyle = grd;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const redraw = (canvas) => {
+  canvas.ctx.clearRect(0, 0, canvas.domCanvas.width, canvas.domCanvas.height);
+  canvas.setCanvasBackground();
+  generate(canvas.ctx);
+}
 
-const startPos = [10, 100];
-const buildingGenerator = new BuildingGenerator(...startPos);
-const buildingDrawer = new BuildingDrawer(ctx, buildingGenerator);
-buildingDrawer.setInitialColor([231, 44, 74]);
-buildingDrawer.drawCity();
+const domContentLoaded = () => {
+  const canvas = new Canvas('canvas');
+  redraw(canvas)
+
+  const generateBtn = document.querySelector('.generate-btn');
+  generateBtn.addEventListener('click', () => redraw(canvas));
+};
+
+document.addEventListener('DOMContentLoaded', domContentLoaded);
